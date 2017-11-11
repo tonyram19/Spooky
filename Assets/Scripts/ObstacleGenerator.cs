31b6ef8313cd860dd30112d918ba488696df17d2
Tony Ramirez <tonyram20@gmail.com>
2017-11-11 18:18:40 +0200

    Initial commit

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject stalactitePrefab;
+    [SerializeField] float generationInterval;
+    [SerializeField] float separation;
+    [SerializeField] float[] stalactiteYPositions;
+    [SerializeField] Vector2[] stalactiteScales;
+
+
+    float timer;
+    Vector2 position;
+    int prevX = 5;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= generationInterval)
+        {
+            GenerateStalactite();
+            timer = 0;
+        }
+
+        Vector2 position = GetComponent<RectTransform>().anchoredPosition;
+
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(position.x - 1 * Time.deltaTime, position.y);
+    }
+
+    public void GenerateStalactite()
+    {
+        GameObject stalactite = Instantiate(stalactitePrefab);
+
+        int random = Random.Range(0, 2);
+
+        stalactite.transform.SetParent(gameObject.transform);
+        stalactite.GetComponent<Transform>().localScale = stalactiteScales[random];
+        stalactite.GetComponent<Transform>().localPosition = new Vector2(separation + prevX, stalactiteYPositions[random]);
+        prevX = (int)stalactite.GetComponent<Transform>().localPosition.x;
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	[SerializeField] float bumpForce;
+	Rigidbody2D myBody;
+
+	void Start () 
+	{
+		myBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			Vector2 force = new Vector2(0, bumpForce);
+			myBody.AddForce(force);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Obstacle")
+		{
+			GetComponent<BoxCollider2D>().enabled = false;
+			GetComponent<Animator>().Play("Explode");
+		}
+	}
+}
